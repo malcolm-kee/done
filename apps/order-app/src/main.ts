d4a5c5d6b13f4ac77d0d0ad34dd72140726be73d
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-02-16 23:51:43 +0800

    enable cors

--- apps/order-app/src/main.ts
+++ apps/order-app/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
   });
+  app.enableCors();
   app.connectMicroservice({
     transport: Transport.REDIS,
     options: {
