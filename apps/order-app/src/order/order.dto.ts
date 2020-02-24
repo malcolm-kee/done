f2728ae02515d7b3312494bed0568120499c1c20
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-02-24 11:00:33 +0800

    add validation and docs (#1)

--- apps/order-app/src/order/order.dto.ts
+++ apps/order-app/src/order/order.dto.ts
@@ -1,4 +1,19 @@
+import { IsIn, IsNotEmpty, IsPositive, IsString } from 'class-validator';
+
 export class CreateOrderDto {
+  @IsString()
+  @IsNotEmpty()
   readonly userId: string;
+
+  @IsPositive()
   readonly total: number;
+}
+
+export class UpdateOrderStatusWithPaymentDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly id: string;
+
+  @IsIn(['confirmed', 'declined'])
+  readonly result: 'confirmed' | 'declined';
 }
